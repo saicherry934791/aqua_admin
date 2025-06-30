7c5d7aad7392fb73ca0e08a699a0e51a8c307e07
saicherry934791 <162865505+saicherry934791@users.noreply.github.com>
2025-06-30 10:20:46 +0530

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@react-navigation/elements';
 import { router, useNavigation } from 'expo-router';
-import { Edit3, TrendingUp, Users, Package, DollarSign, Wrench, MapPin, Calendar, Bell, Activity } from 'lucide-react-native';
+import { LocationEdit as Edit3, TrendingUp, Users, Package, DollarSign, Wrench, MapPin, Calendar, Bell, Activity } from 'lucide-react-native';
 import React, { useContext, useLayoutEffect, useState } from 'react';
 import { ScrollView, StyleSheet, Text, TouchableOpacity, View, Dimensions } from 'react-native';
 import { SheetManager } from 'react-native-actions-sheet';
